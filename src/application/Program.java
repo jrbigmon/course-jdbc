69df0a35eae3d22c1b29bfafa3ea2565fbd1f483
jrbigmon <juniorterriaga0@gmail.com>
2023-12-16 19:58:15 -0300

    test: department service test get list

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import models.services.DepartmentService;
 import models.services.SellerService;
 
 public class Program {
@@ -10,9 +11,12 @@ public class Program {
     Connection connection = DB.getConnection();
 
     SellerService sellerService = new SellerService(connection, "seller");
+    DepartmentService departmentService = new DepartmentService(connection, "department");
 
     sellerService.getList(null, null)
         .forEach(System.out::println);
+
+    departmentService.getList(null, null).forEach(System.out::println);
 
     DB.closeConnection();
   }
