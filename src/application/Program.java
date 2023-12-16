a9638c7888eced18b70fcc1516327582bb9f9a9b
jrbigmon <juniorterriaga0@gmail.com>
2023-12-16 14:24:29 -0300

    test: test get sellers in database

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,15 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import models.services.SellerService;
 
 public class Program {
   public static void main(String[] args) throws Exception {
     Connection connection = DB.getConnection();
+
+    SellerService sellerService = new SellerService(connection);
+
+    sellerService.getList(10).forEach(System.out::println);
 
     DB.closeConnection();
   }
