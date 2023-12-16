4124de8a8960d65cb4386804db41d4f1d4437a64
jrbigmon <juniorterriaga0@gmail.com>
2023-12-16 19:45:57 -0300

    feat: SQLQuery with get list method

---
+++ src/models/utils/SQLQuery.java
@@ -0,0 +1,27 @@
+package models.utils;
+
+import java.util.Arrays;
+
+public class SQLQuery {
+  private SQLQuery() {
+    throw new IllegalStateException("SQLQuery is not initialized");
+  }
+
+  private static String replaceBrackets(String value) {
+    return value.replace("[", "").replace("]", "");
+  }
+
+  public static String getList(String tableName, String[] columns, String where, String[] joins, Integer limit) {
+    if (tableName == null) {
+      throw new IllegalArgumentException("tableName is required");
+    }
+
+    String columnsString = columns != null ? replaceBrackets(Arrays.toString(columns)) : "*";
+    String joinsString = joins != null ? replaceBrackets(Arrays.toString(joins)) : "";
+    String whereString = where != null ? where.replace(";", "") : "";
+    String limitString = limit != null ? "limit " + limit : "";
+
+    return "SELECT " + columnsString + " FROM " + tableName + " " + joinsString + " " + whereString + " " + limitString;
+  }
+
+}
