fd4815c190da83bc6672c993746abb8e1309e858
jrbigmon <juniorterriaga0@gmail.com>
2023-12-16 21:00:59 -0300

    feat: mount sql to create

--- src/models/utils/SQLQuery.java
+++ src/models/utils/SQLQuery.java
@@ -11,6 +11,19 @@ public class SQLQuery {
     return value.replace("[", "").replace("]", "");
   }
 
+  private static String getReplacements(Integer columnsLength) {
+    String replacements = "(";
+
+    for (int i = 0; i < columnsLength; i++) {
+      replacements += "?";
+      if (i != columnsLength - 1) {
+        replacements += ",";
+      }
+    }
+
+    return replacements += ")";
+  }
+
   public static String getList(String tableName, String[] columns, String where, String[] joins, Integer limit) {
     if (tableName == null) {
       throw new IllegalArgumentException("tableName is required");
@@ -24,4 +37,14 @@ public class SQLQuery {
     return "SELECT " + columnsString + " FROM " + tableName + " " + joinsString + " " + whereString + " " + limitString;
   }
 
+  public static String create(String tableName, String[] columns) {
+    return "INSERT INTO "
+        + tableName
+        + " "
+        + "(" + replaceBrackets(Arrays.toString(columns)) + ")"
+        + " "
+        + "VALUES "
+        + getReplacements(columns.length)
+        + ";";
+  }
 }
