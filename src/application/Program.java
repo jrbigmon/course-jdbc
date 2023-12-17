5f8d851f65a5d40b8198b2b65c59e50ebfd849cf
jrbigmon <juniorterriaga0@gmail.com>
2023-12-16 21:01:30 -0300

    test: test create a new seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import models.entities.Department;
 import models.services.DepartmentService;
 import models.services.SellerService;
 
@@ -11,12 +12,24 @@ public class Program {
     Connection connection = DB.getConnection();
 
     SellerService sellerService = new SellerService(connection, "seller");
-    DepartmentService departmentService = new DepartmentService(connection, "department");
+    DepartmentService departmentService = new DepartmentService(connection,
+        "department");
+
+    var departments = departmentService.getList(null, null);
+
+    Department computerDepartment = departments.filter(d -> d.getName().equals("Computers")).findFirst().orElse(null);
+
+    Integer sellerIdCreated = null;
+    if (computerDepartment != null) {
+      sellerIdCreated = sellerService.create("Vagner", "vagner@mail.com",
+          "20/10/1997", 2000.00,
+          computerDepartment.getId());
+    }
+
+    System.out.println("Seller id created: " + sellerIdCreated);
 
     sellerService.getList(null, null)
         .forEach(System.out::println);
-
-    departmentService.getList(null, null).forEach(System.out::println);
 
     DB.closeConnection();
   }
