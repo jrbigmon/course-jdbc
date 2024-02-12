95dd0c05cdc3a575105bffad8187d3de1c34f704
jrbigmon <juniorterriaga0@gmail.com>
2024-02-12 14:05:53 -0300

    feat: utils class created

---
+++ src/models/utils/Utils.java
@@ -0,0 +1,11 @@
+package models.utils;
+
+public class Utils {
+  private Utils() {
+    throw new IllegalStateException("Utils is not initialized");
+  }
+
+  public static Double incrementPercent(Double value, Double percent) {
+    return value + value * percent;
+  }
+}
